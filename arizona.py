9c2ac91b1c84edc7bfcf4cafad4c01c6a292e042
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-05 23:34:12 -0700

    Create arizona.py

---
+++ arizona.py
@@ -0,0 +1,15 @@
+outerWidth=int(input("USS Arizona outer width:\n"))
+
+innerwidth=int(input("USS Arizona inner width:\n"))
+
+towerWidth=int(input("USS Arizona tower height:\n"))
+
+print('                          |      |')
+print('                       |##$      $##|')
+print('                        ##        ##')
+print('                       #..#      #..#')
+print('                \----. #..#......#..# .----/')
+print('          \ ***|_|    |#..#-#-#-##..#|    |_|*** /')
+print('.__________|____          _......_          ____|______..')
+print('`---.                                                   --\.')
+print(' <<#\_______________________________________________________\')
