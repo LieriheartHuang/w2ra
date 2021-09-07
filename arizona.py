43832d2ace60c1ea5b280f99be6aaa621c28b419
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-07 16:32:36 -0700

    Update arizona.py

--- arizona.py
+++ arizona.py
@@ -4,7 +4,10 @@ b=int(input("USS Arizona inner width: \n"))
 
 c=int(input("USS Arizona tower height: \n"))
 d=((a+b)/7)
+for i in range(0,d): # 根据因子迭代
+      d=d-1
 print('      '+a*' '+'                    |'+b*'  '+'|')
+ break
 print('      '+a*' '+'                 |##$'+b*'  '+'$##|')
 print('      '+a*' '+'                  ## '+b*'  '+' ##')
 print('      '+a*' '+'                 #..#'+b*'  '+'#..#')
