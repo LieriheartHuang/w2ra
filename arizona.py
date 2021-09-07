052c49112cbc008509acf6dcaf7cd8191bcad71b
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-07 15:45:56 -0700

    Update arizona.py

--- arizona.py
+++ arizona.py
@@ -1,10 +1,9 @@
-outerWidth=int(input("USS Arizona outer width: \n"))
+a=int(input("USS Arizona outer width: \n"))
 
-innerwidth=int(input("USS Arizona inner width: \n"))
+b=int(input("USS Arizona inner width: \n"))
 
-towerWidth=int(input("USS Arizona tower height: \n"))
-
-highet=((outerWidth + innerWidth)/7)
+c=int(input("USS Arizona tower height: \n"))
+d=((a+b)/7)
 print('      '+outwidth*' '+'                    |'+innerWidth*'  '+'|')
 print('      '+outwidth*' '+'                 |##$'+innerWidth*'  '+'$##|')
 print('      '+outwidth*' '+'                  ## '+innerWidth*'  '+' ##')
@@ -14,4 +13,19 @@ print('      '+outwidth*' '+'    \ ***|_|    |#..#'+innerWidth*'-#'+'#..#|    |_
 print('._____'+outwidth*'_'+'_____|____          _'+innerWidth*'..'+'._          ____|______..')
 print('`---. '+outwidth*' '+'                     '+innerWidth*'  '+'                     --\.')
 print(' <<#\_'+outwidth*'_'+'_____________________'+innerWidth*'__'+'_________________________\')
+      
+### 
+### Author:wenjunhuang
+### Class: CSc 110
+### Description: Describe your program with one
+###              or more sentences of text.
+###
+
+print(".-----------"+"-"*a+".")
+print("| ### ||  ###"+"#"*a+"\\")
+print("| ### ||  "+"#"*a+"####\.")
+print("D     ||  "+" "*a+"<>    |------+")
+print("|  ______ "+" "*a+"     /______ |")
+print(" \/ /..\ \_"+"_"*a+"____/ /..\ \|")
+print("    \__/  "+" "*a+"       \__/")
       
