a7c351662ef6784a9ba53ed1848e393da2cd25d5
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-07 15:33:02 -0700

    Update arizona.py

--- arizona.py
+++ arizona.py
@@ -1,9 +1,10 @@
-outerWidth=int(input("USS Arizona outer width:\n"))
+outerWidth=int(input("USS Arizona outer width: \n"))
 
-innerwidth=int(input("USS Arizona inner width:\n"))
+innerwidth=int(input("USS Arizona inner width: \n"))
 
-towerWidth=int(input("USS Arizona tower height:\n"))
+towerWidth=int(input("USS Arizona tower height: \n"))
 
+highet=((outerWidth + innerWidth)/7)
 print('                          |      |')
 print('                       |##$      $##|')
 print('                        ##        ##')
