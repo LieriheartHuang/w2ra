90fab2aef871d7cd6fbe0096634311d2855b8ff1
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-07 21:13:07 -0700

    Update flyer.py

--- flyer.py
+++ flyer.py
@@ -1,19 +1,19 @@
-a=int(input("Wright flyer size: \"))
-            b=int(a\5)
-            c=int(a*1.2)
-print('   '+c*'     '+      #')
+a=int(input("Wright flyer size: \n"))
+b=int( a/5 )+1
+c=int(a*1.2)
+print('   '+c*'     '+'      #')
 print('   '+c*'     '+'#---------#')
 print('   '+c*'     '+'#---------#')
 print('==='+c*'====='+'=========='+c*'====='+'====')
-for i in range(b)
-print(' H'+c*'    |'+'  |  %H%  |  '+c*|    'H')
+for i in range(b):
+    print(' H'+c*'    |'+'  |  %H%  |  '+c*'|    '+'H')
+          
+print(' H'+c*'    |'+'**|**%H%**|**'+c*'|    '+'H')
       
-print(' H'+c*'    |'+'**|**%H%**|**'+c*|    'H')
-      
-for j in range(b)
-print(' H'+c*'    |'+'  |  %H%  |  '+c*|    'H')
+for j in range(b):
+    print(' H'+c*'    |'+'  |  %H%  |  '+c*'|    '+'H')
       
       
-print('====='+c*'====='+'==%H%==+c*'====='+=====')
+print('====='+c*'====='+'==%H%=='+c*'====='+'=====')
 print('   '+c*'     '+'|         |')
 print('   '+c*'     '+'+#########+')
